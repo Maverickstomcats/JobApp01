9a082e744e82618def304d540e98ba89e709c5a4
Contreras Avila <jbcontreras@cps.k12.il.us>
2024-03-18 16:19:56 -0500

    new changes

--- app.py
+++ app.py
@@ -2,7 +2,20 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+class Form(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    first_name= db.column(db.String(80))
+    last_name= db.column(db.String(80))
+    email= db.column(db.String(80))
+    date= db.column(db.Date)
+    occupation = db.Column(db.String(80))
+
+    
 @app.route("/",methods=["GET","POST"])
 
 def index():
